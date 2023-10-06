0bd36e6e1ef78a9017d233551570ef15b658b498
himanshu <royalhmj@gmail.com>
2023-10-07 04:29:42 +0530

    07.10

--- gamehub/src/Modules/Landing.js
+++ gamehub/src/Modules/Landing.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import Home from '../Componets/Home';
 import Main from '../Componets/Main';
 import { useState } from 'react';
-const socket = io.connect("http://192.168.1.4:5000");
+const socket = io.connect("https://backend-of-game.onrender.com");
 const Landing =() => {
   const [visible, setvisible] = useState(true);
   const [room, setroom] = useState("");
